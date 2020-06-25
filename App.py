41f2b68f8dd53a2850d532822baa0b23b4e76618
Mikhailo <LuKray21@gmail.com>
2020-06-25 12:57:58 +0300

    test

--- App.py
+++ App.py
@@ -17,29 +17,43 @@ class App:
         sc = pygame.display.set_mode((W, H))
         clock = pygame.time.Clock()
         
-        self.player = Player(100,100, "sprites\player")
+        self.player = Player(100,100, "sprites/player")
+        self.terrain = Terrain(100, 100)
+        # rect1 = pygame.Rect((0, 0, 30, 30))
+        # rect1.
 
         while 1:
-        
         
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
                     exit()
                 elif i.type == pygame.KEYDOWN:
-                    if i.key == pygame.K_LEFT:
-                        self.player.move_left()
-                    elif i.key == pygame.K_RIGHT:
-                        self.player.move_right()
-                    elif i.key == pygame.K_UP:
-                        self.player.move_up()
-                    elif i.key == pygame.K_DOWN:
-                        self.player.move_down()
-        
+                    self.player.update(i.key)
+                    CollisionDetector.check_collision(self.player, self.terrain)
+                    
             sc.fill(WHITE)
+            sc.blit(self.terrain.image, self.terrain.rect)
             sc.blit(self.player.image, self.player.rect)
+
             pygame.display.update()
             clock.tick(FPS)
 
+class CollisionDetector():
+    def __init__(self, first, second):
+        pass
+    @classmethod
+    def check_collision(cls, first, second):
+        if pygame.sprite.collide_rect(first, second) == 1:
+            print('COLLISION')
+        
+        
+
+class Terrain(pygame.sprite.Sprite):
+    def __init__(self, x ,y):
+        self.image = pygame.image.load("sprites/terrain/grass_terrain.png").convert() #load a sprite image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 class ObjectActionModel():
     def __init__(self, spriteFolder):
@@ -57,7 +71,6 @@ class ObjectActionModel():
         
         files = os.listdir(spriteFolder)
         for file_name in files:
-            # file_name = file_name[:-4]
             if 'down' in file_name:
                 if 'rest' in file_name:
                     down_rest = spriteFolder + "/" + file_name
@@ -122,18 +135,33 @@ class Human(pygame.sprite.Sprite):
         self.rect.x = pos_x
         self.rect.y = pos_y
 
+    def update(self, event):
+        if event == pygame.K_LEFT:
+            self.move_left()
+        elif event == pygame.K_RIGHT:
+            self.move_right()
+        elif event == pygame.K_UP:
+            self.move_up()
+        elif event == pygame.K_DOWN:
+            self.move_down()
+
     def move_up(self):
         self.image = pygame.image.load(self.moving_model.upMovingSprite()).convert_alpha()
-        self.rect.y -= 5
+        self.rect.y -= 10
     def move_down(self):
         self.image = pygame.image.load(self.moving_model.downMovingSprite()).convert_alpha()
-        self.rect.y += 5
+        self.rect.y += 10
     def move_left(self):
         self.image = pygame.image.load(self.moving_model.leftMovingSprite()).convert_alpha()
-        self.rect.x -= 5
+        self.rect.x -= 10
     def move_right(self):
         self.image = pygame.image.load(self.moving_model.rightMovingSprite()).convert_alpha()
-        self.rect.x += 5
+        self.rect.x += 10
+
+    def checkCollision(self, terrain):
+        col = pygame.sprite.collide_rect(self.rect, terrain)
+        if col == True:
+            print('hi')
 
 class Player(Human):
     def __init__(self, pos_x, pos_y, spriteFolder):
